8fd5229e4e29da436c95902b973ae5213167ed80
huangguangda <2397923107@qq.com>
2017-12-01 17:21:37 +0800

    102

--- app/src/main/java/cn/edu/gdmec/android/uiwidgettest/MainActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/uiwidgettest/MainActivity.java
@@ -1,11 +1,15 @@
 package cn.edu.gdmec.android.uiwidgettest;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +17,10 @@ public class MainActivity extends AppCompatActivity {
     private Button button;
 
     private EditText editText;
+
+    private ImageView imageView;
+
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,12 +31,45 @@ public class MainActivity extends AppCompatActivity {
 
         editText = (EditText) findViewById ( R.id.edit_text );
 
+        imageView = (ImageView) findViewById ( R.id.image_view );
+
+        progressBar = (ProgressBar) findViewById ( R.id.progress_bar );
+
         button.setOnClickListener ( new View.OnClickListener () {
             @Override
             public void onClick(View view) {
                 //在此处添加逻辑
-                String inputText = editText.getText ().toString ();
-                Toast.makeText ( MainActivity.this, inputText, Toast.LENGTH_SHORT ).show ();
+                /*String inputText = editText.getText ().toString ();
+                Toast.makeText ( MainActivity.this, inputText, Toast.LENGTH_SHORT ).show ();*/
+//                imageView.setImageResource ( R.drawable.jelly_bean );
+
+                /*if (progressBar.getVisibility () == View.GONE){
+                    progressBar.setVisibility ( View.VISIBLE );
+                }else {
+                    progressBar.setVisibility ( View.GONE );
+                }*/
+               /* int progress = progressBar.getProgress ();
+                progress = progress + 10;
+                progressBar.setProgress ( progress );*/
+
+                AlertDialog.Builder dialog = new AlertDialog.Builder ( MainActivity.this );
+                dialog.setTitle ( "This is Dialog" );
+                dialog.setMessage ( "Something important." );
+                dialog.setCancelable ( false );
+                dialog.setPositiveButton ( "OK", new DialogInterface.OnClickListener (){
+                    @Override
+                    public void onClick(DialogInterface dialog, int which){
+
+                    }
+                } );
+               dialog.setNegativeButton ( "Cancel", new DialogInterface.OnClickListener (){
+                   @Override
+                   public void onClick(DialogInterface dialog, int which){
+
+                   }
+               } );
+                dialog.show ();
+
             }
         } );
 
